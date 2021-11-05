07badf8e938b87e0d63915d901146e3d284f91bd
KarpushkinS <c1e2r3g@gmail.com>
2021-11-06 01:17:52 +0300

    Added Layer Moving Script

--- LuckyDay/Assets/Scripts/LayerMoving.cs
+++ LuckyDay/Assets/Scripts/LayerMoving.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class LayerMoving : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private Collider2D _collider2D;
     
     void Start()
     {
+        float height = spriteRenderer.sprite.rect.height;
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sortingOrder = -(int)(transform.position.y * 100);
+        //_collider2D = GetComponent<Collider2D>();
+        //spriteRenderer.sortingOrder = -(int) (_collider2D.transform.position.y * 100);
+        spriteRenderer.sortingOrder -= (int)(height / 2.0f);
     }
     
 }
