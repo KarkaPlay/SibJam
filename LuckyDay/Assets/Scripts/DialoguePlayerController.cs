bc0d5f6254408175c840520164cc0a4a6ce87be2
aigerimalmazbekkyzy <71431806+aigerimalmazbekkyzy@users.noreply.github.com>
2021-11-07 00:50:19 +0300

    Добавлена анимация персонажа + подруги

--- LuckyDay/Assets/Scripts/DialoguePlayerController.cs
+++ LuckyDay/Assets/Scripts/DialoguePlayerController.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class DialoguePlayerController: MonoBehaviour
 {
-    public float speed;
-
-    public Sprite[] directions;
     private SpriteRenderer spriteRenderer;
 
     public GameObject pressEToInteract;
     private GameObject nearestInteractive;
     private DialogueTrigger trigger;
+
+    public Animator animator;
 
     void Start()
     {
@@ -27,31 +26,6 @@ public class DialoguePlayerController: MonoBehaviour
             trigger = nearestInteractive.GetComponent<DialogueTrigger>();
             trigger.TriggerDialogue();
         }
-    }
-
-    void FixedUpdate()
-    {
-        //if isGameActive
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
-        // Перемещение
-        var inputX = Input.GetAxis("Horizontal");
-        var inputY = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector2.right * (inputX * speed * Time.fixedDeltaTime), Space.World);
-        transform.Translate(Vector2.up * (inputY * speed * Time.fixedDeltaTime), Space.World);
-
-        if (inputX > 0.2)
-            spriteRenderer.sprite = directions[3]; // Right
-        else if (inputX < -0.2)
-            spriteRenderer.sprite = directions[1]; // Left
-        else if (inputY > 0.2)
-            spriteRenderer.sprite = directions[2]; // Up
-        else if (inputY < -0.2)
-            spriteRenderer.sprite = directions[0]; // Down
     }
 
     private void OnTriggerStay2D(Collider2D other)
