9a71eb64fcfb04f7c621ac5b5c90cc4ecb9f14f0
Гузель Муртазина <nezukomur@gmail.com>
2021-11-07 14:09:40 +0300

    Попытка1 добавления сохранения сцен

--- LuckyDay/Assets/Scripts/MainMenu.cs
+++ LuckyDay/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
 
 public class MainMenu : MonoBehaviour
 {
+    public int sceneid;
+
+    private Save sv = new Save();
+    private string path;
     public void PlayGame()
     {
+        path = Path.Combine(Application.dataPath, "Save.json");//считывание
+        if (File.Exists(path))//проверка данных в файле
+        {
+            sv = JsonUtility.FromJson<Save>(File.ReadAllText(path));
+            Debug.Log("Well" + sv.sceneid);
+        }
+        sceneid = sv.sceneid;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    [Serializable]
+    public class Save
+    {
+        public int sceneid;
+    }
 }
